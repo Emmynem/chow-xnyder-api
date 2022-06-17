38aa867c08daf088c6fc07cce6323edb1cf15e4d
Emmanuel Nwoye <emmanuelnwoye5@gmail.com>
2022-06-18 00:17:16 +0100

    added bank_accounts in main/server/vendor/data

---
+++ server/vendor/data/bank_accounts/add_new_vendor_bank_account.php
@@ -0,0 +1,141 @@
+<?php
+
+  $http_origin = isset($_SERVER['HTTP_ORIGIN']) ? $_SERVER['HTTP_ORIGIN'] : null;
+  $allowed_domains = array("https://auth.reestoc.com", "https://dashboard.reestoc.com");
+  foreach ($allowed_domains as $value) {if ($http_origin === $value) {header('Access-Control-Allow-Origin: ' . $http_origin);}}
+  header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+  header("Access-Control-Allow-Headers: Content-Type, Authorization, origin, Accept-Language, Range, X-Requested-With");
+  header("Access-Control-Allow-Credentials: true");
+  require '../../../config/connect_to_me.php';
+  include_once "../../../config/functions.php";
+
+  class genericClass {
+    public $engineMessage = 0;
+    public $engineError = 0;
+    public $engineErrorMessage;
+    public $resultData;
+    public $filteredData;
+  }
+
+  $data = json_decode(file_get_contents("php://input"), true);
+
+  $functions = new Functions();
+
+  if ($connected) {
+
+    try {
+      $conn->beginTransaction();
+
+      $date_added = $functions->today;
+      $active = $functions->active;
+
+      $user_unique_id = isset($_GET['user_unique_id']) ? $_GET['user_unique_id'] : $data['user_unique_id'];
+      $vendor_unique_id = isset($_GET['vendor_unique_id']) ? $_GET['vendor_unique_id'] : $data['vendor_unique_id'];
+      $account_name = isset($_GET['account_name']) ? $_GET['account_name'] : $data['account_name'];
+      $account_number = isset($_GET['account_number']) ? $_GET['account_number'] : $data['account_number'];
+      $bank = isset($_GET['bank']) ? $_GET['bank'] : $data['bank'];
+      $role = "Owner";
+
+      $sqlSearchUser = "SELECT unique_id FROM vendor_users WHERE unique_id=:unique_id AND vendor_unique_id=:vendor_unique_id AND role=:role AND status=:status";
+      $querySearchUser = $conn->prepare($sqlSearchUser);
+      $querySearchUser->bindParam(":unique_id", $user_unique_id);
+      $querySearchUser->bindParam(":vendor_unique_id", $vendor_unique_id);
+      $querySearchUser->bindParam(":role", $role);
+      $querySearchUser->bindParam(":status", $active);
+      $querySearchUser->execute();
+
+      if ($querySearchUser->rowCount() > 0) {
+
+        $validation = $functions->vendor_account_validation($account_name, $account_number, $bank);
+
+        if ($validation["error"] == true) {
+          $returnvalue = new genericClass();
+          $returnvalue->engineError = 2;
+          $returnvalue->engineErrorMessage = $validation["message"];
+        }
+        else {
+
+          $bank_account_count = $functions->check_length_3;
+          $not_allowed_values = $functions->not_allowed_values;
+          $null = $functions->null;
+
+          $sql2 = "SELECT bank FROM vendor_bank_accounts WHERE vendor_unique_id=:vendor_unique_id AND status=:status";
+          $query2 = $conn->prepare($sql2);
+          $query2->bindParam(":vendor_unique_id", $vendor_unique_id);
+          $query2->bindParam(":status", $active);
+          $query2->execute();
+
+          $sql3 = "SELECT bank FROM vendor_bank_accounts WHERE vendor_unique_id=:vendor_unique_id AND account_name=:account_name AND account_number=:account_number AND bank=:bank AND status=:status";
+          $query3 = $conn->prepare($sql3);
+          $query3->bindParam(":vendor_unique_id", $vendor_unique_id);
+          $query3->bindParam(":account_name", $account_name);
+          $query3->bindParam(":account_number", $account_number);
+          $query3->bindParam(":bank", $bank);
+          $query3->bindParam(":status", $active);
+          $query3->execute();
+
+          if ($query2->rowCount() >= $bank_account_count) {
+            $returnvalue = new genericClass();
+            $returnvalue->engineError = 2;
+            $returnvalue->engineErrorMessage = "Bank account limit reached";
+          }
+          else if ($query3->rowCount() > 0) {
+            $returnvalue = new genericClass();
+            $returnvalue->engineError = 2;
+            $returnvalue->engineErrorMessage = "Bank account already exists";
+          }
+          else {
+            $unique_id = $functions->random_str(20);
+
+            $default_status = $query2->rowCount() == 0 ? "Yes" : "No";
+
+            $sql = "INSERT INTO vendor_bank_accounts (unique_id, vendor_unique_id, account_name, account_number, bank, default_status, added_date, last_modified, status)
+            VALUES (:unique_id, :vendor_unique_id, :account_name, :account_number, :bank, :default_status, :added_date, :last_modified, :status)";
+            $query = $conn->prepare($sql);
+            $query->bindParam(":unique_id", $unique_id);
+            $query->bindParam(":vendor_unique_id", $vendor_unique_id);
+            $query->bindParam(":account_name", $account_name);
+            $query->bindParam(":account_number", $account_number);
+            $query->bindParam(":bank", $bank);
+            $query->bindParam(":default_status", $default_status);
+            $query->bindParam(":added_date", $date_added);
+            $query->bindParam(":last_modified", $date_added);
+            $query->bindParam(":status", $active);
+            $query->execute();
+
+            if ($query->rowCount() > 0) {
+              $returnvalue = new genericClass();
+              $returnvalue->engineMessage = 1;
+            }
+            else{
+              $returnvalue = new genericClass();
+              $returnvalue->engineError = 2;
+              $returnvalue->engineErrorMessage = "Not inserted (new vendor bank account)";
+            }
+          }
+
+        }
+
+      }
+      else {
+        $returnvalue = new genericClass();
+        $returnvalue->engineError = 2;
+        $returnvalue->engineErrorMessage = "Vendor user not found";
+      }
+
+      $conn->commit();
+    } catch (PDOException $e) {
+      $conn->rollback();
+      throw $e;
+    }
+
+  }
+  else {
+    $returnvalue = new genericClass();
+    $returnvalue->engineError = 3;
+    $returnvalue->engineErrorMessage = "No connection";
+  }
+
+  echo json_encode($returnvalue);
+
+?>
