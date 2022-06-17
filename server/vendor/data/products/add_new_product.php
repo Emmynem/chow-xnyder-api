4fb71738fa15188baae6a7f753d95fd7654171b1
Emmanuel Nwoye <emmanuelnwoye5@gmail.com>
2022-06-18 00:18:22 +0100

    added products in main/server/vendor/data

---
+++ server/vendor/data/products/add_new_product.php
@@ -0,0 +1,182 @@
+<?php
+
+  $http_origin = isset($_SERVER['HTTP_ORIGIN']) ? $_SERVER['HTTP_ORIGIN'] : null;
+  $allowed_domains = array("https://auth.reestoc.com", "https://dashboard.reestoc.com");
+  foreach ($allowed_domains as $value) {if ($http_origin === $value) {header('Access-Control-Allow-Origin: ' . $http_origin);}}
+  header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+  header("Access-Control-Allow-Headers: Content-Type, Authorization, origin, Accept-Language, Range, X-Requested-With");
+  header("Access-Control-Allow-Credentials: true");
+  require '../../../config/connect_to_me.php';
+  include_once "../../../config/functions.php";
+
+  class genericClass {
+    public $engineMessage = 0;
+    public $engineError = 0;
+    public $engineErrorMessage;
+    public $resultData;
+    public $filteredData;
+  }
+
+  $data = json_decode(file_get_contents("php://input"), true);
+
+  $functions = new Functions();
+
+  if ($connected) {
+
+    try {
+      $conn->beginTransaction();
+
+      $date_added = $functions->today;
+      $active = $functions->active;
+      $not_allowed_values = $functions->not_allowed_values;
+
+      $user_unique_id = isset($_GET['user_unique_id']) ? $_GET['user_unique_id'] : $data['user_unique_id'];
+      $vendor_unique_id = isset($_GET['vendor_unique_id']) ? $_GET['vendor_unique_id'] : $data['vendor_unique_id'];
+      $menu_unique_id = isset($_GET['menu_unique_id']) ? $_GET['menu_unique_id'] : $data['menu_unique_id'];
+      $category_unique_id = isset($_GET['category_unique_id']) ? $_GET['category_unique_id'] : $data['category_unique_id'];
+      $name = isset($_GET['name']) ? $_GET['name'] : $data['name'];
+      $description = isset($_GET['description']) ? $_GET['description'] : $data['description'];
+      $duration = isset($_GET['duration']) ? $_GET['duration'] : $data['duration'];
+      $weight = isset($_GET['weight']) ? $_GET['weight'] : $data['weight'];
+      $description = isset($_GET['description']) ? $_GET['description'] : $data['description'];
+      $stock = isset($_GET['stock']) ? $_GET['stock'] : $data['stock'];
+      $price = isset($_GET['price']) ? $_GET['price'] : $data['price'];
+      $sales_price = isset($_GET['sales_price']) ? $_GET['sales_price'] : $data['sales_price'];
+
+      $sqlSearchUser = "SELECT unique_id FROM vendor_users WHERE unique_id=:unique_id AND vendor_unique_id=:vendor_unique_id AND status=:status";
+      $querySearchUser = $conn->prepare($sqlSearchUser);
+      $querySearchUser->bindParam(":unique_id", $user_unique_id);
+      $querySearchUser->bindParam(":vendor_unique_id", $vendor_unique_id);
+      $querySearchUser->bindParam(":status", $active);
+      $querySearchUser->execute();
+
+      if ($querySearchUser->rowCount() > 0) {
+
+        $validation = $functions->vendor_product_validation($category_unique_id, $name, $description, $duration, $weight, $stock, $price, $sales_price);
+
+        if ($validation["error"] == true) {
+          $returnvalue = new genericClass();
+          $returnvalue->engineError = 2;
+          $returnvalue->engineErrorMessage = $validation["message"];
+        }
+        else {
+
+          $sql3 = "SELECT unique_id FROM categories WHERE unique_id=:unique_id AND status=:status";
+          $query3 = $conn->prepare($sql3);
+          $query3->bindParam(":unique_id", $category_unique_id);
+          $query3->bindParam(":status", $active);
+          $query3->execute();
+
+          if ($query3->rowCount() > 0) {
+
+            $start = $functions->start;
+            $zero = $functions->zero;
+            $null = $functions->null;
+            $stripped = $functions->strip_text($name);
+
+            $menu_unique_id_alt = in_array($menu_unique_id,$not_allowed_values) ? $null : $menu_unique_id;
+            $duration_alt = in_array($duration,$not_allowed_values) ? $null : $duration;
+            $weight_alt = in_array($weight,$not_allowed_values) ? $null : $weight;
+
+            $sql4 = "SELECT unique_id FROM menus WHERE unique_id=:unique_id AND vendor_unique_id=:vendor_unique_id AND status=:status";
+            $query4 = $conn->prepare($sql4);
+            $query4->bindParam(":unique_id", $menu_unique_id);
+            $query4->bindParam(":vendor_unique_id", $vendor_unique_id);
+            $query4->bindParam(":status", $active);
+            $query4->execute();
+
+            $sql2 = "SELECT name FROM products WHERE vendor_unique_id=:vendor_unique_id AND (menu_unique_id=:menu_unique_id OR menu_unique_id IS NULL) AND category_unique_id=:category_unique_id
+            AND (name=:name OR stripped=:stripped) AND status=:status";
+            $query2 = $conn->prepare($sql2);
+            $query2->bindParam(":vendor_unique_id", $vendor_unique_id);
+            $query2->bindParam(":menu_unique_id", $menu_unique_id);
+            $query2->bindParam(":category_unique_id", $category_unique_id);
+            $query2->bindParam(":name", $name);
+            $query2->bindParam(":stripped", $stripped);
+            $query2->bindParam(":status", $active);
+            $query2->execute();
+
+            if (!$query4->rowCount() > 0 && $menu_unique_id_alt != null) {
+              $returnvalue = new genericClass();
+              $returnvalue->engineError = 2;
+              $returnvalue->engineErrorMessage = "Vendor menu not found";
+            }
+            else if ($query2->rowCount() > 0) {
+              $returnvalue = new genericClass();
+              $returnvalue->engineError = 2;
+              $returnvalue->engineErrorMessage = "Item already exists";
+            }
+            else {
+              $unique_id = $functions->random_str(20);
+
+              $sql = "INSERT INTO products (unique_id, user_unique_id, vendor_unique_id, menu_unique_id, category_unique_id, name, stripped,
+              description, duration, weight, stock, stock_remaining, price, sales_price, views, favorites, good_rating, bad_rating, added_date, last_modified, status)
+              VALUES (:unique_id, :user_unique_id, :vendor_unique_id, :menu_unique_id, :category_unique_id, :name, :stripped,
+              :description, :duration, :weight, :stock, :stock_remaining, :price, :sales_price, :views, :favorites, :good_rating, :bad_rating, :added_date, :last_modified, :status)";
+              $query = $conn->prepare($sql);
+              $query->bindParam(":unique_id", $unique_id);
+              $query->bindParam(":user_unique_id", $user_unique_id);
+              $query->bindParam(":vendor_unique_id", $vendor_unique_id);
+              $query->bindParam(":menu_unique_id", $menu_unique_id_alt);
+              $query->bindParam(":category_unique_id", $category_unique_id);
+              $query->bindParam(":name", $name);
+              $query->bindParam(":stripped", $stripped);
+              $query->bindParam(":description", $description);
+              $query->bindParam(":duration", $duration_alt);
+              $query->bindParam(":weight", $weight_alt);
+              $query->bindParam(":stock", $stock);
+              $query->bindParam(":stock_remaining", $stock);
+              $query->bindParam(":price", $price);
+              $query->bindParam(":sales_price", $sales_price);
+              $query->bindParam(":views", $start);
+              $query->bindParam(":favorites", $start);
+              $query->bindParam(":good_rating", $zero);
+              $query->bindParam(":bad_rating", $zero);
+              $query->bindParam(":added_date", $date_added);
+              $query->bindParam(":last_modified", $date_added);
+              $query->bindParam(":status", $active);
+              $query->execute();
+
+              if ($query->rowCount() > 0) {
+                $returnvalue = new genericClass();
+                $returnvalue->engineMessage = 1;
+              }
+              else{
+                $returnvalue = new genericClass();
+                $returnvalue->engineError = 2;
+                $returnvalue->engineErrorMessage = "Not inserted (new item)";
+              }
+            }
+
+          }
+          else {
+            $returnvalue = new genericClass();
+            $returnvalue->engineError = 2;
+            $returnvalue->engineErrorMessage = "Category not found";
+          }
+
+        }
+
+      }
+      else {
+        $returnvalue = new genericClass();
+        $returnvalue->engineError = 2;
+        $returnvalue->engineErrorMessage = "Vendor user not found";
+      }
+
+      $conn->commit();
+    } catch (PDOException $e) {
+      $conn->rollback();
+      throw $e;
+    }
+
+  }
+  else {
+    $returnvalue = new genericClass();
+    $returnvalue->engineError = 3;
+    $returnvalue->engineErrorMessage = "No connection";
+  }
+
+  echo json_encode($returnvalue);
+
+?>
---
+++ server/vendor/data/products/update_product_details.php
@@ -0,0 +1,159 @@
+<?php
+
+  $http_origin = isset($_SERVER['HTTP_ORIGIN']) ? $_SERVER['HTTP_ORIGIN'] : null;
+  $allowed_domains = array("https://auth.reestoc.com", "https://dashboard.reestoc.com");
+  foreach ($allowed_domains as $value) {if ($http_origin === $value) {header('Access-Control-Allow-Origin: ' . $http_origin);}}
+  header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+  header("Access-Control-Allow-Headers: Content-Type, Authorization, origin, Accept-Language, Range, X-Requested-With");
+  header("Access-Control-Allow-Credentials: true");
+  require '../../../config/connect_to_me.php';
+  include_once "../../../config/functions.php";
+
+  class genericClass {
+    public $engineMessage = 0;
+    public $engineError = 0;
+    public $engineErrorMessage;
+    public $resultData;
+    public $filteredData;
+  }
+
+
+  $data = json_decode(file_get_contents("php://input"), true);
+
+  $functions = new Functions();
+
+  if ($connected) {
+
+    try {
+      $conn->beginTransaction();
+
+      $date_added = $functions->today;
+      $active = $functions->active;
+      $not_active = $functions->not_active;
+      $null = $functions->null;
+      $not_allowed_values = $functions->not_allowed_values;
+
+      $unique_id = isset($_GET['unique_id']) ? $_GET['unique_id'] : $data['unique_id'];
+      $user_unique_id = isset($_GET['user_unique_id']) ? $_GET['user_unique_id'] : $data['user_unique_id'];
+      $vendor_unique_id = isset($_GET['vendor_unique_id']) ? $_GET['vendor_unique_id'] : $data['vendor_unique_id'];
+      $menu_unique_id = isset($_GET['menu_unique_id']) ? $_GET['menu_unique_id'] : $data['menu_unique_id'];
+      $category_unique_id = isset($_GET['category_unique_id']) ? $_GET['category_unique_id'] : $data['category_unique_id'];
+      $name = isset($_GET['name']) ? $_GET['name'] : $data['name'];
+      $description = isset($_GET['description']) ? $_GET['description'] : $data['description'];
+      $duration = isset($_GET['duration']) ? $_GET['duration'] : $data['duration'];
+      $weight = isset($_GET['weight']) ? $_GET['weight'] : $data['weight'];
+      $price = isset($_GET['price']) ? $_GET['price'] : $data['price'];
+      $sales_price = isset($_GET['sales_price']) ? $_GET['sales_price'] : $data['sales_price'];
+
+      $sqlSearchUser = "SELECT unique_id FROM vendor_users WHERE unique_id=:unique_id AND vendor_unique_id=:vendor_unique_id AND status=:status";
+      $querySearchUser = $conn->prepare($sqlSearchUser);
+      $querySearchUser->bindParam(":unique_id", $user_unique_id);
+      $querySearchUser->bindParam(":vendor_unique_id", $vendor_unique_id);
+      $querySearchUser->bindParam(":status", $active);
+      $querySearchUser->execute();
+
+      if ($querySearchUser->rowCount() > 0) {
+
+        $validation = $functions->update_vendor_product_validation($category_unique_id, $name, $description, $duration, $weight, $price, $sales_price);
+
+        if ($validation["error"] == true) {
+          $returnvalue = new genericClass();
+          $returnvalue->engineError = 2;
+          $returnvalue->engineErrorMessage = $validation["message"];
+        }
+        else {
+
+          $stripped = $functions->strip_text($name);
+
+          $sql2 = "SELECT unique_id FROM products WHERE unique_id=:unique_id AND vendor_unique_id=:vendor_unique_id AND status=:status";
+          $query2 = $conn->prepare($sql2);
+          $query2->bindParam(":unique_id", $unique_id);
+          $query2->bindParam(":vendor_unique_id", $vendor_unique_id);
+          $query2->bindParam(":status", $active);
+          $query2->execute();
+
+          if ($query2->rowCount() > 0) {
+
+            $sql3 = "SELECT name FROM products WHERE vendor_unique_id=:vendor_unique_id AND (menu_unique_id=:menu_unique_id OR menu_unique_id IS NULL)
+            AND category_unique_id=:category_unique_id AND (name=:name OR stripped=:stripped) AND unique_id!=:unique_id AND status=:status";
+            $query3 = $conn->prepare($sql3);
+            $query3->bindParam(":unique_id", $unique_id);
+            $query3->bindParam(":vendor_unique_id", $vendor_unique_id);
+            $query3->bindParam(":menu_unique_id", $menu_unique_id);
+            $query3->bindParam(":category_unique_id", $category_unique_id);
+            $query3->bindParam(":name", $name);
+            $query3->bindParam(":stripped", $stripped);
+            $query3->bindParam(":status", $active);
+            $query3->execute();
+
+            if ($query3->rowCount() > 0) {
+              $returnvalue = new genericClass();
+              $returnvalue->engineError = 2;
+              $returnvalue->engineErrorMessage = "Item name already exists";
+            }
+            else {
+
+              $menu_unique_id_alt = in_array($menu_unique_id,$not_allowed_values) ? $null : $menu_unique_id;
+              $duration_alt = in_array($duration,$not_allowed_values) ? $null : $duration;
+              $weight_alt = in_array($weight,$not_allowed_values) ? $null : $weight;
+
+              $sql = "UPDATE products SET menu_unique_id=:menu_unique_id, category_unique_id=:category_unique_id,
+              name=:name, stripped=:stripped, description=:description, duration=:duration, weight=:weight, price=:price, sales_price=:sales_price, last_modified=:last_modified WHERE unique_id=:unique_id AND vendor_unique_id=:vendor_unique_id";
+              $query = $conn->prepare($sql);
+              $query->bindParam(":unique_id", $unique_id);
+              $query->bindParam(":vendor_unique_id", $vendor_unique_id);
+              $query->bindParam(":menu_unique_id", $menu_unique_id_alt);
+              $query->bindParam(":category_unique_id", $category_unique_id);
+              $query->bindParam(":name", $name);
+              $query->bindParam(":stripped", $stripped);
+              $query->bindParam(":description", $description);
+              $query->bindParam(":duration", $duration_alt);
+              $query->bindParam(":weight", $weight_alt);
+              $query->bindParam(":price", $price);
+              $query->bindParam(":sales_price", $sales_price);
+              $query->bindParam(":last_modified", $date_added);
+              $query->execute();
+
+              if ($query->rowCount() > 0) {
+                $returnvalue = new genericClass();
+                $returnvalue->engineMessage = 1;
+              }
+              else {
+                $returnvalue = new genericClass();
+                $returnvalue->engineError = 2;
+                $returnvalue->engineErrorMessage = "Not edited (item details)";
+              }
+
+            }
+
+          }
+          else {
+            $returnvalue = new genericClass();
+            $returnvalue->engineError = 2;
+            $returnvalue->engineErrorMessage = "Item not found";
+          }
+        }
+
+      }
+      else {
+        $returnvalue = new genericClass();
+        $returnvalue->engineError = 2;
+        $returnvalue->engineErrorMessage = "Vendor user not found";
+      }
+
+      $conn->commit();
+    } catch (PDOException $e) {
+      $conn->rollback();
+      throw $e;
+    }
+
+  }
+  else {
+    $returnvalue = new genericClass();
+    $returnvalue->engineError = 2;
+    $returnvalue->engineErrorMessage = "No connection";
+  }
+
+  echo json_encode($returnvalue);
+
+?>
